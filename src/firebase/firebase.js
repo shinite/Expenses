4be2d2dc4244279925270603b29ac8d854860937
shinite <ssshambhavi8@gmail.com>
2018-02-05 18:19:42 +0530

    Setup database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBvgqNt4I99pTVZM2Df-4IDnrdxGdqIJGs",
-    authDomain: "expenses-87d0d.firebaseapp.com",
-    databaseURL: "https://expenses-87d0d.firebaseio.com",
-    projectId: "expenses-87d0d",
-    storageBucket: "expenses-87d0d.appspot.com",
-    messagingSenderId: "1078350160858"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL:process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 
