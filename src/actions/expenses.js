488c025850476694b3e318ff702bc1f98b28886a
shinite <ssshambhavi8@gmail.com>
2018-02-06 00:02:01 +0530

    setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -35,6 +35,14 @@ export const removeExpense = ({id} = {}) => ({
   type:'REMOVE_EXPENSE',
   id
 });
+
+export const startRemoveExpense = ({id}={}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(()=>{
+      dispatch(removeExpense({id}))
+    })
+  }
+}
 //EDIT
 
 export const editExpense = (id, updates) => ({
